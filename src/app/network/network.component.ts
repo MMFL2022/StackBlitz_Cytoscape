84651b6167097a5ea9acd474cb7f3188b2cbce9e
MMFL2022 <5269786+MMFL2022@users.noreply.github.com>
2023-04-05 19:04:23 -0400

    1 file modified

--- src/app/network/network.component.ts
+++ src/app/network/network.component.ts
@@ -24,7 +24,8 @@ export class NetworkComponent implements AfterViewInit {
           css: {
             'content': 'data(id)',
             'text-valign': 'center',
-            'text-halign': 'center'
+            'text-halign': 'center',
+            'label': 'data(name)'
           }
         },
         {
@@ -32,6 +33,7 @@ export class NetworkComponent implements AfterViewInit {
           css: {
             'text-valign': 'top',
             'text-halign': 'center',
+            'label': 'data(name)'
           }
         },
         {
@@ -39,24 +41,29 @@ export class NetworkComponent implements AfterViewInit {
           css: {
             'width': 'data(width)',
             'curve-style': 'bezier',
-            'target-arrow-shape': 'triangle'
+            'target-arrow-shape': 'triangle',
+            'label': 'data(name)'
           }
         }
       ],
 
       elements: {
         nodes: [
-          { data: { id: 'C1' } },
-          { data: { id: 'iC1.1', parent: 'C1' }, position: { x: 10, y: 10} },
-          { data: { id: 'iC1.2', parent: 'C1' }, position: { x: 90, y: 10} },
-          { data: { id: 'oC1', parent: 'C1' }, position: { x: 50, y: 50} },
-          { data: { id: 'C2' } },
-          { data: { id: 'iC2.1', parent: 'C2' }, position: { x: 20, y: 150} },
-          { data: { id: 'iC2.2', parent: 'C2' }, position: { x: 100, y: 150} },
-          { data: { id: 'oC2', parent: 'C2' }, position: { x: 60, y: 200} },
+          { data: { id: 'i1', name: 'Treatment Depth' }, position: { x: 10, y: 10} },
+          { data: { id: 'i2', name: 'Penetration Depth' }, position: { x: 90, y: 10} },
+          { data: { id: 'C1', name: 'Maximum' } },
+          { data: { id: 'iC1.1', name: 'i:maxA', parent: 'C1' }, position: { x: 10, y: 100} },
+          { data: { id: 'iC1.2', name: 'i:maxB', parent: 'C1' }, position: { x: 90, y: 100} },
+          { data: { id: 'oC1', name: 'o:maxV', parent: 'C1' }, position: { x: 50, y: 150} },
+          { data: { id: 'C2', name: 'Cascade2' } },
+          { data: { id: 'iC2.1', name: 'i:funA', parent: 'C2' }, position: { x: 20, y: 250} },
+          { data: { id: 'iC2.2', name: 'i:funB', parent: 'C2' }, position: { x: 100, y: 250} },
+          { data: { id: 'oC2', name: 'o:funV', parent: 'C2' }, position: { x: 60, y: 300} },
         ],
         edges: [
-          { data: { id: 'oC1-iC2.1', source: 'oC1', target: 'iC2.1', width: 1 } },
+          { data: { id: 'i1-iC1.1', name: '', source: 'i1', target: 'iC1.1', width: 1 } },
+          { data: { id: 'i2-iC1.1', name: '', source: 'i2', target: 'iC1.2', width: 1 } },
+          { data: { id: 'oC1-iC2.1', name: '', source: 'oC1', target: 'iC2.1', width: 1 } },
         ]
       },
 
